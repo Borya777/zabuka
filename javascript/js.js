3e9d5b21f0e6237d0fe861f806b9fcdb6837a36b
Borya777 <86748624+Borya777@users.noreply.github.com>
2023-03-29 00:48:59 +0300

    Add files via upload

--- javascript/js.js
+++ javascript/js.js
@@ -1,18 +1,13 @@
+var menuBar = false;
+
+
+
 
 var InputMainMenu = document.querySelector('form>div>input');
 var ButtonSearch = document.querySelector('form>button');
 /*alert(InputMainMenu.text);
 alert(InputMainMenu.textContent);
 alert(InputMainMenu.attributes);*/
-
-
-function pauseBrowser(millis) {
-    var date = Date.now();
-    var curDate = null;
-    do {
-        curDate = Date.now();
-    } while (curDate-date < millis);
-}
 
 
 
@@ -27,19 +22,71 @@ for (var key in attributes) {
 
 
 
-function alerts(value){InputMainMenu.style.opacity = value}
+function alerts(value) {if (menuBar == false) { InputMainMenu.style.opacity = value}}
 
-function fadeOutRight(quSelector){
+function focusOnInput(){input.focus()}
+
+function widthShrinIn(quSelector){
 	
-	var  selected = document.querySelector(quSelector);
-
-	document.querySelector(quSelector).style.animationName = "fadeOutRight"
-	//pauseBrowser(10000);
-	//document.querySelector(quSelector).style.display = 'none'
+	
+	var  selected = document.querySelectorAll(quSelector);
+	selected.forEach(function(userItem) {
+		userItem.style.animationName = "widthShrinIn"
+	})
 }
 
 
+function widthShrinOut(quSelector){
+	
+	
+	var  selected = document.querySelectorAll(quSelector);
+	selected.forEach(function(userItem) {
+		userItem.style.animationName = "widthShrinOut"
+	})
+	
+}
+
+function checkMenu(){
+	if (menuBar == false) {menuBar = true; document.querySelector('.form-control.me-2').focus()}
+	else {alert(document.querySelector('.form-control.me-2').value)}
+	
+}
 
 
-/*getAttributes(InputMainMenu)*/
+var input = document.querySelector('.form-control.me-2')
+//alert(input)
+input.addEventListener('focus', (event) => {
+	if (menuBar != false){
+	//alert(document.activeElement.tagName)
+  event.target.style.opacity = "1";
+  event.target.style.width = "100%"
+	}
+  
+  //alert(menuBar)
+  
+});
 
+function returnAll(){
+	input.style.opacity = "0";
+	input.style.width = "5px"
+	var  selected = document.querySelectorAll('.widthShrinOut');
+	selected.forEach(function(userItem) {
+		userItem.style.animationName = "widthShrinIn"
+	})
+	
+		var  selected = document.querySelectorAll('.widthShrin');
+	selected.forEach(function(userItem) {
+		userItem.style.animationName = "widthShrinOut"
+	})
+	menuBar = false;
+	
+	var  selected = document.querySelectorAll('.btn.btn-outline-success');
+	selected.forEach(function(userItem) {
+		userItem.style.overflow = "inherit";
+		userItem.style.padding = '5px';
+		userItem.style.margin = '5px';
+	})
+	
+}
+
+//document.addEventListener('focus', (event) => {alert(document.activeElement.tagName)});
